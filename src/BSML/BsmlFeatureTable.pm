20b9598a5b0e4c8f176b33d8379217d55e3ec0ae
chauser <chauser@061605af-e407-0410-a945-9c8a22772c06>
2003-02-28 21:38:48 +0000

    added pod documentation

--- src/BSML/BsmlFeatureTable.pm
+++ src/BSML/BsmlFeatureTable.pm
@@ -1,6 +1,40 @@
 package BsmlFeatureTable;
 @ISA = qw( BsmlElement );
 
+=head1 NAME
+
+BsmlFeatureTable.pm - Bsml Feature Table Class
+
+=head1 VERSION
+
+This document refers to version 1.0 of the BSML Object Layer
+
+=head1 SYNOPSIS
+
+  Adding a feature and reference to a feature table
+
+  my $feature = $table->returnBsmlFeatureR( $table->addBsmlFeature() );
+  my $ref = $table->returnBsmlReferenceR( $table->addBsmlReference() );
+
+=head1 DESCRIPTION
+
+=head2 Overview
+
+  The BsmlFeatureTable class provides methods and storage for Bsml Feature and Resource elements.
+
+=head2 Constructor and initialization
+
+  Typically a BsmlFeatureTable is created by the BsmlSequence object it is contained within and
+  manipulated as a reference.
+
+  my $FTable = $seq->returnFeatureTableR( addBsmlFeatureTable() );
+
+=head2 Class and object methods
+
+=over 4
+
+=cut
+
 use BsmlElement;
 use XML::Writer;
 use BsmlFeature;
@@ -31,6 +65,16 @@ sub init
 
   }
 
+=item $FTable->addBsmlFeature()
+
+B<DESCRIPTION:> adds a feature to the feature table
+
+B<Parameters:> None
+
+B<Returns:> index of the created feature (position of the feature in the feature list)
+
+=cut
+
 sub addBsmlFeature
   {
     my $self = shift;
@@ -39,6 +83,16 @@ sub addBsmlFeature
     my $index = @{$self->{'BsmlFeatures'}} - 1;
     return $index;
   }
+
+=item $FTable->dropBsmlFeature( $index )
+
+B<DESCRIPTION:> removes a feature in the feature table with given index
+
+B<Parameters:> $index, position of the feature in the feature list
+
+B<Returns:> None
+
+=cut
 
 sub dropBsmlFeature
   {
@@ -58,12 +112,32 @@ sub dropBsmlFeature
     $self->{'BsmlFeatures'} = $newlist;
   }
 
+=item $FTable->returnBsmlFeatureListR()
+
+B<DESCRIPTION:> returns a reference to the list of feature objects contained in the feature table
+
+B<Parameters:> None
+
+B<Returns:> list reference
+
+=cut
+
 sub returnBsmlFeatureListR
   {
     my $self = shift;
 
     return $self->{'BsmlFeatures'};
   }
+
+=item $FTable->returnBsmlFeatureR( $index )
+
+B<DESCRIPTION:> returns a reference to the BsmlFeature at position $index in the feature list
+
+B<Parameters:> $index - the feature's position in the feature list
+
+B<Returns:> reference to a BsmlFeature
+
+=cut
 
 sub returnBsmlFeatureR
   {
@@ -73,6 +147,16 @@ sub returnBsmlFeatureR
     return $self->{'BsmlFeatures'}[$index];
   }
 
+=item $FTable->addBsmlReference()
+
+B<DESCRIPTION:> adds a reference to the feature table
+
+B<Parameters:> None
+
+B<Returns:> index of the created reference (position of the reference in the reference list)
+
+=cut
+
 sub addBsmlReference
   {
     my $self = shift;
@@ -81,6 +165,16 @@ sub addBsmlReference
     my $index = @{$self->{'BsmlReferences'}} - 1;
     return $index;
   }
+
+=item $FTable->dropBsmlReference( $index )
+
+B<DESCRIPTION:> removes a reference in the reference table with given index
+
+B<Parameters:> $index, position of the reference in the reference list
+
+B<Returns:> None
+
+=cut
 
 sub dropBsmlReference
   {
@@ -100,12 +194,32 @@ sub dropBsmlReference
     $self->{'BsmlReferences'} = $newlist;
   }
 
+=item $FTable->returnBsmlReferenceListR()
+
+B<DESCRIPTION:> returns a reference to the list of BsmlReference objects contained in the feature table
+
+B<Parameters:> None
+
+B<Returns:> reference to a list of BsmlReferences
+
+=cut
+
 sub returnBsmlReferenceListR
   {
     my $self = shift;
     
     return $self->{'BsmlReferences'};
   }
+
+=item $FTable->returnBsmlReferenceR( $index )
+
+B<DESCRIPTION:> returns a reference to the BsmlReference at position $index in the reference list
+
+B<Parameters:> $index - the reference's position in the reference list
+
+B<Returns:> reference to a BsmlReference
+
+=cut
 
 sub returnBsmlReferenceR
   {
@@ -114,6 +228,16 @@ sub returnBsmlReferenceR
 
     return $self->{'BsmlReferences'}[$index];
   }
+
+=item $seq->write()
+
+  B<Description:> writes the BSML elements encoded by the class to a file using XML::Writer. This method should only be called through the BsmlDoc->write() process.
+
+  B<Parameters:> None
+
+  B<Returns:> None
+
+=cut 
 
 sub write
   {
