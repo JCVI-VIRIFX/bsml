541c15f4e91528b39c2a23aa04e95f8dd7c9ca90
chauser <chauser@061605af-e407-0410-a945-9c8a22772c06>
2003-03-18 21:48:04 +0000

    updates related to BsmlReader

--- test/BsmlReaderTest.pl
+++ test/BsmlReaderTest.pl
@@ -10,6 +10,11 @@ my $parser = new BsmlParserTwig;
 
 $parser->parse( \$reader, $ARGV[0] );
 
+print "Done Parsing!\n";
+
+exit();
+
+
 my $seqs = $reader->returnAllSequences();
 
 foreach my $seq ( @{$seqs} )
