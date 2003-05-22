697e8ca21cd3714b862a8de33d73c060a2d7c322
chauser <chauser@061605af-e407-0410-a945-9c8a22772c06>
2003-05-22 17:46:23 +0000

    demostration of BSML::BsmlParserSerialSearch

---
+++ test/BsmlSerialSearch-Example.pl
@@ -0,0 +1,75 @@
+#! /local/perl/bin/perl
+
+use lib '../src/';
+use BSML::BsmlParserSerialSearch;
+use BSML::BsmlSeqPairAlignment;
+use BSML::BsmlReader;
+use Data::Dumper;
+
+# A reader object can be used to "package" the data contained in BsmlSequence, BsmlSeqPairAlignment, and BsmlAnalysis objects
+# into structured hashes. 
+
+my $reader = new BSML::BsmlReader;
+
+# The constructor for the serial parser accepts three optional arguments. Each of these arguments defines a reference to a callback function.
+# The parser will call these functions with an object reference at parse time as element is encoutered. 
+
+my $parser = new BSML::BsmlParserSerialSearch( AlignmentCallBack => \&alignmentPrint, AnalysisCallBack => \&analysisPrint, SequenceCallBack => \&sequencePrint );
+
+# Begin BSML parse. If no filename is provided the parser defaults to STDIN. 
+
+if( $ARGV[0] ){
+    $parser->parse( $ARGV[0] );}
+else{
+    $parser->parse();}
+
+
+# Callback function for alignment objects demonstrating low-level object usage.
+
+sub alignmentPrint
+{
+    my $alnref = shift;    
+    print "ALN: ";
+
+    # Element attributes can be returned using the returnattr method.
+    # Equivalently BSML Attributes ie. <Attribute name="percent_identity" content="100"/> can
+    # be returned with returnBsmlAttr(). Both are defined in the BsmlElement base class.
+
+    # Print the reference ids of the query and subject sequences.
+    print $alnref->returnattr( 'refseq' );
+    print " ";
+    print $alnref->returnattr( 'compseq' );
+    print "\n";
+
+    # An array of BSML::SeqPairRun objects is stored within each BSML::SeqPairAlignment. It can
+    # be accessed with the following method.
+
+    my $runs = $alnref->returnBsmlSeqPairRunListR();
+
+    # Print the start position of each run relative to the query sequence.
+
+    print "   -> ";
+    foreach my $seqpairrun ( @{$runs} )
+    {
+	print $seqpairrun->returnattr( 'refpos' )." ";
+    }
+    print "\n";
+}
+
+# A callback function for the analysis object demonstrating the usage of the BsmlReader class.
+
+sub analysisPrint 
+{
+    my $analysisref = shift;
+    print Dumper( $reader->readAnalysis( $analysisref ) );
+}
+
+# A callback function for sequence objects.
+
+sub sequencePrint
+{
+    my $seqref = shift;
+    print "SEQ: ";
+    print $seqref->returnattr( 'id' );
+    print "\n";
+}
