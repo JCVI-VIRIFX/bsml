79480d8722af95e21668ade18a8f8a90e7fd256d
sundaram <sundaram@061605af-e407-0410-a945-9c8a22772c06>
2003-06-19 21:38:25 +0000

    *** empty log message ***

--- src/BSML/BsmlReader.pm
+++ src/BSML/BsmlReader.pm
@@ -435,6 +435,8 @@ sub readSeqPairAlignment
 	# verify the essential filter parameters
 	#-----------------------------------------------------
 	if ((defined($filter)) && (defined($filter_count))){
+	    print ("filter:$filter\t filter_count:$filter_count\n");
+	    
 	    if ($filter !~ /refpos|runlength|refcomplement|comppos|comprunlength|compcomplement|runscore|runprob/){
 		die "Un-recognized attribute: $filter";
 	    }
