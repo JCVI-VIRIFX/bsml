1ac34834f333b152d70ed12fbaad58ebd80b93e2
chauser <chauser@061605af-e407-0410-a945-9c8a22772c06>
2003-06-26 14:58:21 +0000

    multialignment tests

---
+++ test/BsmlBuilderMultiAlignTest.pl
@@ -0,0 +1,95 @@
+#! /local/perl/bin/perl
+
+# client script to test the functionality of the helper functions defined i
+# BsmlBuilder.pm
+
+# The BsmlBuilder class mirrors the Java API defined in the LabBook Bsml Documentation
+# for building Bsml sequences.
+
+use lib "../src/";
+use BSML::BsmlBuilder;
+use BSML::BsmlReader;
+use BSML::BsmlParserTwig;
+use Data::Dumper;
+
+$outfile = 'multi-out.bsml';
+
+if( $ARGV[0] ){ $outfile = $ARGV[0] };
+
+my $doc = new BSML::BsmlBuilder;
+$doc->makeCurrentDocument();
+
+$doc->createAndAddSequence( 'PF14_0392', 'pfa1 PF14_0392', '', 'dna' );
+$doc->createAndAddSequence( 'PF14_0393', 'pfa1 PF14_0393', '', 'dna' );
+$doc->createAndAddSequence( 'PF14_0394', 'pfa1 PF14_0394', '', 'dna' );
+
+my $table = $doc->createAndAddMultipleAlignmentTable( 'molecule-type', 'nucleotide' );
+my $summary = $doc->createAndAddAlignmentSummary( 'multipleAlignmentTable' => $table,
+						  'seq-type' => 'nucleotide',
+						  'seq-format' => 'FASTA' );
+
+$doc->createAndAddAlignedSequence( 'alignmentSummary' => $summary,
+				   'seqref' => 'PF14_0392',
+				   'seqnum' => '0',
+				   'length' => '512',
+				   'name' => 'pfa1 PF14_0392' );
+
+
+$doc->createAndAddAlignedSequence( 'alignmentSummary' => $summary,
+				   'seqref' => 'PF14_0393',
+				   'seqnum' => '1',
+				   'length' => '512',
+				   'name' => 'pfa1 PF14_0393' );
+
+$doc->createAndAddAlignedSequence( 'alignmentSummary' => $summary,
+				   'seqref' => 'PF14_0394',
+				   'seqnum' => '2',
+				   'length' => '512',
+				   'name' => 'pfa1 PF14_0394' );
+
+my $pairs = $doc->createAndAddPairwiseAlignments( 'multipleAlignmentTable' => $table );
+
+$doc->createAndAddAlignedPair( 'pairwiseAlignments' => $pairs,
+			       'seqnum1' => 0,
+			       'seqnum2' => 1,
+			       'score' => 999 );
+
+$doc->createAndAddAlignedPair( 'pairwiseAlignments' => $pairs,
+			       'seqnum1' => 0,
+			       'seqnum2' => 2,
+			       'score' => 990 );
+
+$doc->createAndAddAlignedPair( 'pairwiseAlignments' => $pairs,
+			       'seqnum1' => 1,
+			       'seqnum2' => 2,
+			       'score' => 980 );
+
+my $aln = $doc->createAndAddSequenceAlignment( 'multipleAlignmentTable' => $table,
+					       'sequences' => 3 );
+
+$doc->createAndAddSequenceData( 'sequenceAlignment' => $aln,
+				'seq-name' => 'pfa1 PF14_0392',
+				'seq-data' => 'AGCTAGCTAGCT------------------------------------------' );
+
+$doc->createAndAddSequenceData( 'sequenceAlignment' => $aln,
+				'seq-name' => 'pfa1 PF14_0393',
+				'seq-data' => 'AGCTAGCTAGCT------------------------------------------' );
+
+$doc->createAndAddSequenceData( 'sequenceAlignment' => $aln,
+				'seq-name' => 'pfa1 PF14_0394',
+				'seq-data' => 'AGCTAGCTAGCT------------------------------------------' );
+
+
+$doc->write( $outfile );
+
+my $reader = new BSML::BsmlReader;
+my $parser = new BSML::BsmlParserTwig;
+
+$parser->parse( \$reader, $outfile );
+
+foreach my $mAln ( @{$reader->returnMultipleAlignmentTables()} )
+{
+    my $href = $reader->readMultipleAlignmentTable( $mAln );
+    print Dumper( %{$href} );
+}
+
--- test/BsmlGeneModel.t
+++ test/BsmlGeneModel.t
@@ -1,12 +1,7 @@
 #! /local/perl/bin/perl
 
-# client script to test the functionality of the helper functions defined i
-# BsmlBuilder.pm
-
-# The BsmlBuilder class mirrors the Java API defined in the LabBook Bsml Documentation
-# for building Bsml sequences.
-
-#use lib "../src/";
+# This does not seem to effect the perl make test routines, however it allows local CVS testing
+use lib "../src/";
 
 use BSML::BsmlBuilder;
 use BSML::BsmlReader;
