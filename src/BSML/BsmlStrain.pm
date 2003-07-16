67fa314b75322509efb8bc5b21dbb0edc687876e
chauser <chauser@061605af-e407-0410-a945-9c8a22772c06>
2003-07-16 18:42:19 +0000

    preliminary support for the BSML Strain object

---
+++ src/BSML/BsmlStrain.pm
@@ -0,0 +1,51 @@
+package BSML::BsmlStrain;
+@ISA = qw( BSML::BsmlElement );
+
+use BSML::BsmlElement;
+use XML::Writer;
+
+use strict;
+use warnings;
+
+sub new
+{
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+
+    $self->init();
+    return $self;
+}
+
+sub init
+{
+    my $self = shift;
+    
+    $self->{'attr'} = {};
+    $self->{'BsmlAttr'} = {};
+    $self->{'BsmlLink'} = [];
+}
+
+sub write
+{
+    my $self = shift;
+    my $writer = shift;
+
+    $writer->startTag( "Strain", %{$self->{'attr'}} );
+
+    foreach my $bsmlattr (keys( %{$self->{ 'BsmlAttr'}}))
+      {
+	$writer->startTag( "Attribute",  'name' => $bsmlattr, 'content' => $self->{'BsmlAttr'}->{$bsmlattr} );
+	$writer->endTag( "Attribute" );
+      }
+
+    foreach my $link (@{$self->{'BsmlLink'}})
+    {
+        $writer->startTag( "Link", %{$link} );
+        $writer->endTag( "Link" );
+    }
+
+    $writer->endTag( "Strain" );
+}
+
+1
