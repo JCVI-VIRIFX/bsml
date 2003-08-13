af7ad766d0679b49806fc9df67ebf5f6d52eea57
angiuoli <angiuoli@061605af-e407-0410-a945-9c8a22772c06>
2003-08-13 21:08:39 +0000

    fixed path problems

--- schema/xsdValid-Xerces/dtdValid.pl
+++ schema/xsdValid-Xerces/dtdValid.pl
@@ -8,14 +8,16 @@ my $results = GetOptions( \%options, 'dtd|d=s' );
 my $file = $ARGV[0];
 my $dtd = $options{'dtd'};
 
+my ($dir) = ($0 =~ /(.*)\/.*/);
+
 if( $dtd )
 {
     $dtd =~ s/\//\\\//g;
-    system "sed -e \'s/<\\!DOCTYPE Bsml PUBLIC \"-\\/\\/EBI\\/\\/Labbook, Inc. BSML DTD\\/\\/EN\" \"http:\\/\\/www.labbook.com\\/dtd\\/bsml3_1.dtd\">/<\\!DOCTYPE Bsml SYSTEM \"$dtd\">/\' $file | ./Xerces-xsdValid";
+    system "sed -e \'s/<\\!DOCTYPE Bsml PUBLIC \"-\\/\\/EBI\\/\\/Labbook, Inc. BSML DTD\\/\\/EN\" \"http:\\/\\/www.labbook.com\\/dtd\\/bsml3_1.dtd\">/<\\!DOCTYPE Bsml SYSTEM \"$dtd\">/\' $file | $dir/Xerces-xsdValid";
 }
 
 else
 {
-    system "more $file | ./Xerces-xsdValid";
+    system "more $file | $dir/Xerces-xsdValid";
 }
 
--- schema/xsdValid-Xerces/xsdValid.pl
+++ schema/xsdValid-Xerces/xsdValid.pl
@@ -12,5 +12,7 @@ my $schema = $options{'schema'};
 
 $schema =~ s/\//\\\//g;
 
-system "sed -e \'s/<\\!DOCTYPE Bsml PUBLIC \"-\\/\\/EBI\\/\\/Labbook, Inc. BSML DTD\\/\\/EN\" \"http:\\/\\/www.labbook.com\\/dtd\\/bsml3_1.dtd\">//\' -e \'s/<Bsml>/<Bsml xmlns:xsi=\"http:\\/\\/www.w3.org\\/2001\\/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"$schema\">/\' $file | ./Xerces-xsdValid";
+my ($dir) = ($0 =~ /(.*)\/.*/);
+
+system "sed -e \'s/<\\!DOCTYPE Bsml PUBLIC \"-\\/\\/EBI\\/\\/Labbook, Inc. BSML DTD\\/\\/EN\" \"http:\\/\\/www.labbook.com\\/dtd\\/bsml3_1.dtd\">//\' -e \'s/<Bsml>/<Bsml xmlns:xsi=\"http:\\/\\/www.w3.org\\/2001\\/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"$schema\">/\' $file | $dir/Xerces-xsdValid";
 
