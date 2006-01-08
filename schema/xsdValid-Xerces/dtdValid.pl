47c84bcb69f168b66422cb1fde4fd4a4da6ad6d7
angiuoli <angiuoli@061605af-e407-0410-a945-9c8a22772c06>
2006-01-08 23:58:56 +0000

    dtd validation re-enabled. replaced xerces with xmllint to perform validation

--- schema/xsdValid-Xerces/dtdValid.pl
+++ schema/xsdValid-Xerces/dtdValid.pl
@@ -19,41 +19,13 @@ if( $options{'help'} || !($ARGV[0]) )
 
 
 ##############################################################################
-## TEMPORARY HACK
-## because of library differences between the servers and desktop machines,
-## this script will return true any time it is run on a machine with the 2.6
-## series kernel.  this should be removed once the servers are upgraded.
-
-my $uname = `uname -r`;
-
-if ($uname =~ /^2.6/) {
-    exit(0);
-}
-else{
-   exit(0);
-}
-
-$ENV{LD_LIBRARY_PATH} = "/usr/local/lib:$ENV{LD_LIBRARY_PATH}";
-
-##############################################################################
 
 my $file = $ARGV[0];
 my $dtd = $options{'dtd'};
 
 my ($dir) = ($0 =~ /(.*)\/.*/);
 
-my $status = 0;
-
-if( $dtd )
-{
-    $dtd =~ s/\//\\\//g;
-    $status = system "sed -e \'s/<\\!DOCTYPE Bsml PUBLIC \"-\\/\\/EBI\\/\\/Labbook, Inc. BSML DTD\\/\\/EN\" \"http:\\/\\/www.labbook.com\\/dtd\\/bsml3_1.dtd\">/<\\!DOCTYPE Bsml SYSTEM \"$dtd\">/\' $file | $dir/Xerces-xsdValid";
-}
-
-else
-{
-    $status = system "more $file | $dir/Xerces-xsdValid";
-}
+my $status = system "xmllint --noout --valid --dtdvalid $dtd $file";
 
 my $exit_value = $status >> 8;
 my $signal_num = $status & 127;
